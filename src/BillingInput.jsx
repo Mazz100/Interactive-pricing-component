d3d17ecf400e3bef8abca9add455db339bcbfd07
Mazen Hassan <95134348+Mazz100@users.noreply.github.com>
2024-03-24 13:52:06 +0200

    Slider range with page and price values update

--- src/BillingInput.jsx
+++ src/BillingInput.jsx
@@ -1,6 +1,16 @@
+import React, { useState, useEffect } from "react";
+
 
 
 function BillingInput() {
+    const [choosePlan, setChoosePlan] = useState('');
+
+    function handlePlanChange(event) {
+        setChoosePlan(event.target.value);
+       
+    }
+
+
     return (
         <fieldset className="billing-filedset">
             <legend className="visually-hidden">Billing</legend>
@@ -11,8 +21,20 @@ function BillingInput() {
 
                 {/*Wrapping inputs with span for styling*/}
                 <span className="toggle-wrapper">
-                    <input className="billing-radio" type="radio" name="Billing" id="Monthly" defaultChecked aria-describedby="monthly toggle switch" />
-                    <input className="billing-radio" type="radio" name="Billing" id="Yearly" aria-describedby="yearly toggle switch" />
+                    <input className="billing-radio" type="radio"
+                        name="Billing"
+                        id="Monthly"
+                        value="Monthly"
+                        onChange={handlePlanChange}
+                        defaultChecked
+                        aria-describedby="monthly toggle switch" />
+
+                    <input className="billing-radio" type="radio"
+                        name="Billing"
+                        id="Yearly"
+                        value="Yearly"
+                        onChange={handlePlanChange}
+                        aria-describedby="yearly toggle switch" />
 
                     {/*Switch and thumb style elements*/}
                     <span className="toggle"></span>
--- src/PricingComponent.jsx
+++ src/PricingComponent.jsx
@@ -5,9 +5,40 @@ import React, { useEffect, useState } from 'react'
 import * as Slider from '@radix-ui/react-slider'
 
 function PricingComponent() {
-    const [page, setPage] = useState(100);
+    const [page, setPage] = useState('100K');
     const [price, setPrice] = useState(16);
 
+
+
+    function handlePage(value) {
+        //Radix slider onValueChange by default recieves the slider value directly
+        let newValue;
+
+        if (value <= 25) {
+            newValue = '10K';
+            setPrice(8);
+        }
+        else if (value <= 50) {
+            newValue = '50K';
+            setPrice(12);
+        }
+        else if (value <= 75) {
+            newValue = '100K';
+            setPrice(16);
+        }
+        else if (value <= 90) {
+            newValue = '500K';
+            setPrice(24);
+        }
+        else {
+            newValue = '1M';
+            setPrice(36);
+        }
+
+        setPage(newValue);
+    }
+
+    console.log(page);
 
     return (
         <>
@@ -15,14 +46,18 @@ function PricingComponent() {
                 <form className='form' action="" name="">
 
                     <p className="page-views-text">
-                        {`${page}K pageviews`}
+                        {page} pageviews
                     </p>
 
                     {/*Pricing Slider*/}
                     <Slider.Root className='sliderRoot'
-                        defaultValue={[3]}
-                        max={5}
-                        step={1}
+                        max={100}
+                        onValueChange={handlePage}
+                        value={[page === '10K' ? 0 :
+                            page === '50K' ? 25 :
+                                page === '100K' ? 50 :
+                                    page === '500K' ? 75 : 100]}
+                        step={10}
                     >
                         <Slider.Track className='sliderTrack'>
                             <Slider.Range className='sliderRange' />
